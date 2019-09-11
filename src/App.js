1cbf91a0be5ac2ea158991ed2dbb8b27c775d5b6
Michael Jurkoic <mjjurkoic@gmail.com>
2019-09-11 01:28:28 -0500

    Home page styling in progress

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Start from "./components/search.component";
+import Home from "./components/home.component";
 import ResultsMap from "./components/results-map.component";
 import Login from "./components/login.component";
 import Hosts from "./components/hosts.component";
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={Start} />
+        <Route exact path="/" component={Home} />
         <Route path="/search" component={Search} />
         <Route path="/map" component={ResultsMap} />
         <Route path="/login" component={Login} />
